ef358600fb907d2f296259cc407548912d5c8cd1
DBOYaoao <yiikai1987910@gmail.com>
2020-08-31 23:32:37 +0800

    rowIndices匹配，不使用暴力匹配（=======7%）

--- SteroSlam.cpp
+++ SteroSlam.cpp
@@ -14,6 +14,17 @@
 #include "slammap.h"
 using namespace MySlam;
 using namespace cv;
+const float SCALE_FACTOR = 1.2f;
+float getScaleFactor(int level)
+{
+    float scale = 1.0f;
+    for(int i = 1; i < level; i++)
+    {
+        scale*=SCALE_FACTOR;
+    }
+    return scale;
+}
+
 int main(int argc, char* * argv)
 {
 
@@ -21,14 +32,40 @@ int main(int argc, char* * argv)
     string img2path = "/home/yiikai/Develop/MySlam/resource/21/image_0/000001.png";
     cv::Mat img1 = cv::imread(img1path);
     cv::Mat img2 = cv::imread(img2path);
-    cv::Ptr<cv::ORB> orb = cv::ORB::create();
-    std::vector<cv::KeyPoint> keypoints1,keypoints2;
-    cv::Mat descriptors1,descriptors2;
-    orb->detectAndCompute(img1,cv::Mat(),keypoints1,descriptors1);
-    orb->detectAndCompute(img2,cv::Mat(),keypoints2,descriptors2);
-    cv::BFMatcher matcher(NORM_HAMMING);
-    std::vector<DMatch> mathces;
-	matcher.match(descriptors1, descriptors2, mathces);
+    cv::Ptr<cv::ORB> orbleft = cv::ORB::create(500,SCALE_FACTOR,8,31,0,2,ORB::FAST_SCORE);
+    cv::Ptr<cv::ORB> orbright = cv::ORB::create(500,SCALE_FACTOR,8,31,0,2,ORB::FAST_SCORE);
+
+    std::vector<cv::KeyPoint> keypointsL,keypointsR;
+    cv::Mat descriptorsL,descriptorsR;
+    orbleft->detectAndCompute(img1,cv::Mat(),keypointsL,descriptorsL);
+    orbright->detectAndCompute(img2,cv::Mat(),keypointsR,descriptorsR);
+    //cv::BFMatcher matcher(NORM_HAMMING);
+    //std::vector<DMatch> mathces;
+	//matcher.match(descriptorsL, descriptorsR, mathces);
+    
+    std::vector<std::vector<int>> vRowIndices(keypointsR.size(), std::vector<int>());
+    for(int i = 0 ; i < keypointsR.size(); i++)
+    {
+        vRowIndices[i].reserve(200);
+    }
+
+    const int rows = img1.rows;
+    for(int iR = 0; iR < keypointsR.size(); iR++)
+    {
+        cv::KeyPoint& kp = keypointsR[iR];
+        const float y = kp.pt.y;
+        const float x = kp.pt.x;
+        const float r = 2.0f * getScaleFactor(kp.octave);
+        const float maxRange = ceil(y + r);
+        const float minRange = floor(y - r);
+        cout<<minRange<<endl;
+        for(int i = minRange; i < maxRange; i++)
+        {
+            vRowIndices[i].push_back(iR);
+        }
+    }
+
+#if 0 //比较粗糙的特征匹配筛选
     //drawKeypoints(img1, keypoints, img1, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_OVER_OUTIMG);
     double min_dist = 10000, max_dist = 0;
     for(int i = 0; i < descriptors1.rows; i++)
@@ -38,7 +75,8 @@ int main(int argc, char* * argv)
         if(mathces[i].distance > max_dist)
             max_dist = mathces[i].distance;
     }
-    cout<<"max distance: "<<max_dist<<endl;
+    c
+    out<<"max distance: "<<max_dist<<endl;
     cout<<"min distance: "<<min_dist<<endl;
     std::vector<DMatch> filterMathces;
     for(int i = 0; i < descriptors1.rows; i++)
@@ -46,10 +84,11 @@ int main(int argc, char* * argv)
         if(mathces[i].distance <= max(2 * min_dist, 100.0))
             filterMathces.push_back(mathces[i]);
     }
-	Mat matchMat;
-	drawMatches(img1, keypoints1, img2, keypoints2, filterMathces, matchMat);
-    imshow("match feature",matchMat);
-    cv::waitKey(0);
+#endif 
+	//Mat matchMat;
+	//drawMatches(img1, keypoints1, img2, keypoints2, filterMathces, matchMat);
+    //imshow("match feature",matchMat);
+    //cv::waitKey(0);
 #if 0
 	string path = "/home/yiikai/Develop/MySlam/resource/21";
 	DataSets sets(path);
